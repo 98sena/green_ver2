4df2c992614afd7868f7efa1e0b2f3627f6f2f46
Kim Yeri <98sena@naver.com>
2020-08-17 00:58:13 +0900

    카메라 BF 각도 수정

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -65,6 +65,7 @@ public class InputManager : MonoBehaviour
             Debug.Log("BFSlope" + slider.value);
             //camera.transform.position = new Vector3(camera.transform.position.x, (9.5f) + (slider.value - 0.5f) * 7f, camera.transform.position.z);
             camera.transform.position = camPlace.transform.position;
+            camera.transform.rotation = camPlace.transform.rotation;
 
             //golfBall.transform.position = new Vector3(golfBall.transform.position.x, 6.25f + (slider.value - 0.5f)*7f, golfBall.transform.position.z);
             golfBall.transform.position = ballPlace.transform.position;
